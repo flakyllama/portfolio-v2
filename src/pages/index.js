2e0b6fff7692379854145e993548425e78e9aa7f
Fardeen Chowdhury <fardeen.h.chowdhury@gmail.com>
2022-06-29 09:17:15 -0400

    Styled footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import '../styles/index.scss'
+import ArrowUpRight from '../images/icons/arrow-up-right.svg'
+import WavingHand from '../images/emojis/waving-hand.svg'
 
 const Landing = () => (
   <>
     <div className="inner">
-      <footer className='footer'>
-        <a>
+      <footer className="footer">
+        <a className="nav-link">
           Process
         </a>
-        <a>
-          Resume
+        <a className="nav-link">
+          <span>
+            Resume
+          </span>
+          <img className="suffix-icon" src={ArrowUpRight} />
         </a>
-        <div className='divider-inline-v' />
-        <a>
-          👋 fardeen.h.chowdhury@gmail.com
+        <div className="divider-inline-v" />
+        <a className="nav-link email-address">
+          <img className="prefix-icon" src={WavingHand} />
+          <span>
+            fardeen.h.chowdhury@gmail.com
+          </span>
         </a>
       </footer>
       </div>
