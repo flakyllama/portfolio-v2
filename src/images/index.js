fe0051a6215a9e26a48ac8329d5a97e0c061c1a6
Fardeen Chowdhury <fardeen.h.chowdhury@gmail.com>
2022-06-29 23:01:41 -0400

    Finished adding content and structure for project sections, mostly completed project 1 - Sobol, added new neutral

---
+++ src/images/index.js
@@ -0,0 +1,29 @@
+/* Icons */
+import ArrowUpRightNeutral from '../images/icons/arrow-up-right-neutral.svg'
+import ArrowUpRightGradient from '../images/icons/arrow-up-right-gradient.svg'
+
+/* Emojis */
+import WavingHand from '../images/emojis/waving-hand.svg'
+
+/* Avatars */
+import Avatar from '../images/personal-avatar.png'
+
+// Sobol
+import BryanPeters from '../images/avatars/sobol/bryan-peters.png'
+import VictorLeipnik from '../images/avatars/sobol/victor-leipnik.png'
+import DamianSarzynski from '../images/avatars/sobol/damian-sarzynski.png'
+import VladKutsenko from '../images/avatars/sobol/vlad-kutsenko.png'
+import LeifDejong from '../images/avatars/sobol/leif-dejong.png'
+
+
+export {
+    ArrowUpRightNeutral,
+    ArrowUpRightGradient,
+    WavingHand,
+    Avatar,
+    BryanPeters,
+    VictorLeipnik,
+    DamianSarzynski,
+    VladKutsenko,
+    LeifDejong
+  };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import '../styles/index.scss'
-/* images */
-import Avatar from '../images/personal-avatar.png'
-import ArrowUpRightNeutral from '../images/icons/arrow-up-right-neutral.svg'
-import ArrowUpRightGradient from '../images/icons/arrow-up-right-gradient.svg'
-import WavingHand from '../images/emojis/waving-hand.svg'
+import '../images/index.js'
+import { 
+  ArrowUpRightNeutral,
+  ArrowUpRightGradient,
+  WavingHand,
+  Avatar,
+  BryanPeters,
+  VictorLeipnik,
+  DamianSarzynski,
+  VladKutsenko,
+  LeifDejong
+} from '../images/index.js'
 
 const Landing = () => (
   <>
@@ -37,6 +44,54 @@ const Landing = () => (
                   As a solo designer, I work  with the product team to deliver value to users, while improving on aesthetics and functionality.
                 </p>
               </div>
+              <div className="project-team">
+                <span className="project-team-label">TEAM</span>
+                <a className="project-team-member" href="https://www.linkedin.com/in/bryan-peters-3125373/" target="_blank" rel="noreferrer">
+                  <img className="project-team-member-avatar" src={BryanPeters} />
+                  <div className="project-team-member-details">
+                    <span>Bryan Peters</span>
+                    <div className="divider-inline-h-round" />
+                    <span className="project-team-member-details-role">Co-founder / Product Manager</span>
+                    <img className="suffix-icon" src={ArrowUpRightGradient} />
+                  </div>
+                </a>
+                <a className="project-team-member" href="https://www.linkedin.com/in/victor-leipnik-2515352" target="_blank" rel="noreferrer">
+                  <img className="project-team-member-avatar" src={VictorLeipnik} />
+                  <div className="project-team-member-details">
+                    <span>Victor Leipnik</span>
+                    <div className="divider-inline-h-round" />
+                    <span className="project-team-member-details-role">Co-founder / Tech Lead</span>
+                    <img className="suffix-icon" src={ArrowUpRightGradient} />
+                  </div>
+                </a> 
+                <a className="project-team-member" href="https://www.linkedin.com/in/dsarzynski/" target="_blank" rel="noreferrer">
+                  <img className="project-team-member-avatar" src={DamianSarzynski} />
+                  <div className="project-team-member-details">
+                    <span>Damian Sarzynski</span>
+                    <div className="divider-inline-h-round" />
+                    <span className="project-team-member-details-role">Product Manager</span>
+                    <img className="suffix-icon" src={ArrowUpRightGradient} />
+                  </div>
+                </a> 
+                <a className="project-team-member" href="https://www.linkedin.com/in/vlad-kutsenko-80994316/" target="_blank" rel="noreferrer">
+                  <img className="project-team-member-avatar" src={VladKutsenko} />
+                  <div className="project-team-member-details">
+                    <span>Vlad Kutsenko</span>
+                    <div className="divider-inline-h-round" />
+                    <span className="project-team-member-details-role">Senior Engineer</span>
+                    <img className="suffix-icon" src={ArrowUpRightGradient} />
+                  </div>
+                </a> 
+                <a className="project-team-member" href="https://www.linkedin.com/in/leifdejong/" target="_blank" rel="noreferrer">
+                  <img className="project-team-member-avatar" src={LeifDejong} />
+                  <div className="project-team-member-details">
+                    <span>Leif Dejong</span>
+                    <div className="divider-inline-h-round" />
+                    <span className="project-team-member-details-role">Full-stack Engineer</span>
+                    <img className="suffix-icon" src={ArrowUpRightGradient} />
+                  </div>
+                </a> 
+              </div>
             </div>
           </div>
         </div>
@@ -51,7 +106,7 @@ const Landing = () => (
           </span>
           <img className="suffix-icon" src={ArrowUpRightNeutral} />
         </a>
-        <div className="divider-inline-v" />
+        <div className="divider-inline-h-rect" />
         <a className="nav-link email-address">
           <img className="prefix-icon" src={WavingHand} />
           <span>
