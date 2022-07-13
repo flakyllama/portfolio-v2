75186c4fe2c30b7643c4835db53a44d04eb8a40a
Fardeen Chowdhury <fardeen.h.chowdhury@gmail.com>
2022-07-13 13:33:31 -0400

    Added gtag plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,23 @@ module.exports = {
       icon: 'src/images/favicon.svg',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-MCVXYSS2YZ" // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
     `gatsby-plugin-sass`],
 }
